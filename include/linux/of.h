1d0251b4af3fa3fedc73f971eefdec876bc40214
Menno van Grinsven <scanno71@gmail.com>
2016-09-08 22:10:58 +0200

    Revert "[1/2] Add patches for Android M by Dmitry Grinberg <dmitrygr@gmail.com>"
    This reverts commit 9ca5e11aafe20312976e26fc35e98b8ee2c25ccc.

--- include/linux/of.h
+++ include/linux/of.h
@@ -183,7 +183,6 @@ extern struct device_node *of_find_matching_node(struct device_node *from,
 #define for_each_matching_node(dn, matches) \
 	for (dn = of_find_matching_node(NULL, matches); dn; \
 	     dn = of_find_matching_node(dn, matches))
-
 extern struct device_node *of_find_node_by_path(const char *path);
 extern struct device_node *of_find_node_by_phandle(phandle handle);
 extern struct device_node *of_get_parent(const struct device_node *node);
@@ -261,11 +260,6 @@ extern void of_detach_node(struct device_node *);
 
 #define of_match_ptr(_ptr)	(_ptr)
 #else /* CONFIG_OF */
-
-static inline struct device_node *of_find_node_by_path(const char *path)
-{
-    return NULL;
-}
 
 static inline bool of_have_populated_dt(void)
 {
